02688a54d03b6daddd8f912d79e927138d04bd07
einbone921 <einbone921@gmail.com>
2021-01-08 15:25:52 +0000

    [Add] マイページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource) #ログイン後にログインユーザーのページへ遷移
+    user_path(resource)
+  end
+
   private
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-
   root to: 'homes#top'
   get '/about' => 'homes#about'
+  resources :users, only: [:show, :edit, :update]
 end
