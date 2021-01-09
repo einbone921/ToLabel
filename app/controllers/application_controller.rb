9b4de6035c17dcffc9416838e6695ddda75a9f29
einbone921 <einbone921@gmail.com>
2021-01-09 03:16:14 +0000

    [Add]rubocopの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource) #ログイン後にログインユーザーのページへ遷移
+  def after_sign_in_path_for(resource) # ログイン後にログインユーザーのページへ遷移
     user_path(resource)
   end
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
   end
 end
