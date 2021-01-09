87e91b616e486989514884e3231b9ab3b03c186c
einbone921 <einbone921@gmail.com>
2021-01-09 08:32:03 +0000

    [Add]ユーザー詳細ページを編集

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,9 +18,16 @@ class PostImagesController < ApplicationController
   end
 
   def edit
+    @post_image = PostImage.find(params[:id])
   end
 
   def update
+    @post_image = PostImage.new(post_image_params)
+    if @post_image.save
+      redirect_to post_image_path(@post_image)
+    else
+      render "edit"
+    end
   end
 
   def destroy
@@ -29,6 +36,6 @@ class PostImagesController < ApplicationController
   private
 
   def post_image_params
-    params.require(:post_image).permit(:post_image, :caption)
+    params.require(:post_image).permit(:post_image_id, :caption)
   end
 end
