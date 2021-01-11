6c177d679ca305a065badda2e1bf9503527dca3a
einbone921 <einbone921@gmail.com>
2021-01-11 12:54:31 +0000

    [Update] コーディング修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  #フォローする側からみた関係
+  # フォローする側からみた関係
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
 
-  #フォローされる側からみた関係
+  # フォローされる側からみた関係
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :following
 
-  #対象のユーザーがフォロー済み(passive_relationships)に含まれているか
+  # 対象のユーザーがフォロー済み(passive_relationships)に含まれているか
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
