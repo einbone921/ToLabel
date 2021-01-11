301d537dadf88ccac73a025c84e84c739b3900af
einbone921 <einbone921@gmail.com>
2021-01-11 05:07:16 +0000

    [Update] 非会員ユーザーの機能を修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,5 @@
 class PostImagesController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @post_image = PostImage.new
