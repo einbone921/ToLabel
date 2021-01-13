4691a0bb8546114d392140fd30c8fa7d3ff93931
einbone921 <einbone921@gmail.com>
2021-01-13 07:20:04 +0000

    [Add] follow機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
     @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: @post_image.id)
     favorite.save
-    # redirect_to request.referer
   end
 
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: @post_image.id)
     favorite.destroy
-    # redirect_to request.referer
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
