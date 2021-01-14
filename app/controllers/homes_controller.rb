6f1afc3e43b8eb49c50056edfab779dea32f1774
einbone921 <einbone921@gmail.com>
2021-01-14 12:55:05 +0000

    [Add] 検索機能の追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,16 @@
 class HomesController < ApplicationController
+  before_action :top
+
   def top
-    @post_images = PostImage.all
     @tags = Tag.all
+    if @q = PostImage.ransack(params[:q])
+      @post_images = @q.result
+    else
+      @post_images = PostImage.all
+    end
   end
 
   def about
   end
+
 end
