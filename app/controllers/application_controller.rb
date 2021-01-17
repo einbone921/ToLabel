55fe2ea0baf823a17756cfef8c9d57234163e29f
einbone921 <einbone921@gmail.com>
2021-01-17 06:45:02 +0000

    [Update] 投稿の一覧表示画面の機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
 
   # ログイン後にログインユーザーのページへ遷移
   def after_sign_in_path_for(resource)
-    # user_path(resource)
     if user_signed_in?
       root_path
     else
