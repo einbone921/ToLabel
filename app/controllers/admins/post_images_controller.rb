63d0dbee871b7d0991d39935b867e33247abf48b
einbone921 <einbone921@gmail.com>
2021-01-17 06:04:58 +0000

    [Add] post_imagesテーブルにis_activeカラムを追加

--- app/controllers/admins/post_images_controller.rb
+++ app/controllers/admins/post_images_controller.rb
@@ -14,12 +14,12 @@ class Admins::PostImagesController < ApplicationController
   def update
     post_image = PostImage.find(params[:id])
     post_image.update(post_image_params)
-    redirect_to "show"
+    redirect_to admins_post_image_path(post_image)
   end
 
   private
 
-  # def post_image_params
-  #   params.require(:post_image).permit()
-  # end
+  def post_image_params
+    params.require(:post_image).permit(:is_active)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_025828) do
+ActiveRecord::Schema.define(version: 2021_01_17_054630) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_01_16_025828) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.boolean "is_active", default: true, null: false
   end
 
   create_table "relationships", force: :cascade do |t|
