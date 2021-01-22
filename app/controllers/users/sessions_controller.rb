8a41304c2d0120c747cdac9d9fbcd676e0dfbd99
einbone921 <einbone921@gmail.com>
2021-01-22 03:39:03 +0000

    [Update] ログイン認証をnameカラムからemailカラムへ変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,7 +13,7 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def reject_user
-    @user = User.find_by(name: params[:user][:name].downcase)
+    @user = User.find_by(email: params[:user][:email].downcase)
     if @user
       if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
         flash[:error] = "退会済みです。"
