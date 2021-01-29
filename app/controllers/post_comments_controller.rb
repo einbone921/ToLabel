0e7011ffa2284863be32d3edbab7fdacbb965ad4
einbone921 <einbone921@gmail.com>
2021-01-29 13:45:06 +0000

    [Update] コメント機能の非同期通信化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,11 @@
 class PostCommentsController < ApplicationController
   def create
-    post_image = PostImage.find(params[:post_image_id])
+    @post_image = PostImage.find(params[:post_image_id])
     comment = PostComment.new(post_comment_params)
     comment.user_id = current_user.id
-    comment.post_image_id = post_image.id
+    comment.post_image_id = @post_image.id
     comment.save
-    redirect_to post_image_path(post_image)
+    # redirect_to post_image_path(post_image)
   end
 
   def destroy
